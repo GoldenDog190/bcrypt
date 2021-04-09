8d5bea1a8277712eb73508023182866d31f571de
Amelia Waggoner <wildginger@wavecable.com>
2021-04-08 17:33:37 -0700

    First commit

---
+++ bcrypt/src/main/java/com/GoldenDog190/bcrypt/BcryptApplication.java
@@ -0,0 +1,13 @@
+package com.GoldenDog190.bcrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcryptApplication.class, args);
+	}
+
+}
